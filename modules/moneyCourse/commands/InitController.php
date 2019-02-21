ee65efa543fb791fc6b39d456d9a67538fb026c6
alexandr <beller.a@mail.ru>
2019-02-21 11:54:34 +0700

    Обновление зависимостей

--- modules/moneyCourse/commands/InitController.php
+++ modules/moneyCourse/commands/InitController.php
@@ -14,67 +14,7 @@ class InitController extends Controller{
      * Запускает обновление курсов валют со сбербанка
      */
     public function actionRun(){
-        $module = \Yii::$app->getModule('mc');
-
-        echo "Send request to get valutes".PHP_EOL;
-
-        $client = new Client();
-        $request = $client->get($module->refreshSberUrl)
-            ->send();
-
-        if(!$request->isOk){
-            echo Console::ansiFormat("Can't get data from url", [Console::FG_YELLOW]).PHP_EOL;
-        }
-
-        echo "Data success get".PHP_EOL;
-
-        if(!array_key_exists('Valute', $request->data)){
-            echo "Data don't have search key ".Console::ansiFormat("Valute", [Console::FG_YELLOW]).PHP_EOL;
-            return false;
-        }
-
-        if(($total = count($request->data['Valute'])) && ($total <= 0)){
-            echo "Amount valute is zero".PHP_EOL;
-            return false;
-        }
-
-
-        $db = \Yii::$app->db;
-        $transaction = $db->beginTransaction();
-
-        try {
-            echo "Clear database moneyCourse" . PHP_EOL;
-            $db->createCommand()->truncateTable(MoneyCourse::tableName());
-
-            echo Console::startProgress(($row = 0), $total, ($startPrefix = 'Update MoneyCourse'));
-
-            foreach ($request->data['Valute'] as $item) {
-
-                $nominal = 1;
-                $moneyItem = new MoneyCourse([
-                    'name' => $item['Name'],
-                    'char' => $item['CharCode'],
-                    'val' => str_replace(',', '.', $item['Value']) / $nominal,
-                    'date' => \Yii::$app->formatter->asDatetime(time(), "Y-MM-dd H:i:s")
-                ]);
-
-                $moneyItem->save();
-
-                $row++;
-                echo Console::updateProgress($row, $total, $startPrefix . " {$moneyItem->char}");
-
-            }
-
-            echo Console::endProgress();
-
-            $transaction->commit();
-
-            echo "Update MoneyCourse success".PHP_EOL;
-
-        }catch (\Exception $exception){
-            $transaction->rollBack();
-        }
-
+        MoneyCourse::refreshValutes();
     }
 
 }
--- modules/moneyCourse/models/MoneyCourse.php
+++ modules/moneyCourse/models/MoneyCourse.php
@@ -3,6 +3,8 @@
 namespace reketaka\helps\modules\moneyCourse\models;
 
 use Yii;
+use yii\helpers\Console;
+use yii\httpclient\Client;
 
 /**
  * This is the model class for table "money_course".
@@ -75,5 +77,68 @@ class MoneyCourse extends \yii\db\ActiveRecord
         $r = $a*$v;
         $r = Yii::$app->formatter->asDecimal($r);
         return $r;
+    }
+
+    public static function refreshValutes(){
+        $module = \Yii::$app->getModule('mc');
+
+        echo "Send request to get valutes".PHP_EOL;
+
+        $client = new Client();
+        $request = $client->get($module->refreshSberUrl)
+            ->send();
+
+        if(!$request->isOk){
+            echo Console::ansiFormat("Can't get data from url", [Console::FG_YELLOW]).PHP_EOL;
+        }
+
+        echo "Data success get".PHP_EOL;
+
+        if(!array_key_exists('Valute', $request->data)){
+            echo "Data don't have search key ".Console::ansiFormat("Valute", [Console::FG_YELLOW]).PHP_EOL;
+            return false;
+        }
+
+        if(($total = count($request->data['Valute'])) && ($total <= 0)){
+            echo "Amount valute is zero".PHP_EOL;
+            return false;
+        }
+
+
+        $db = \Yii::$app->db;
+        $transaction = $db->beginTransaction();
+
+        try {
+            echo "Clear database moneyCourse" . PHP_EOL;
+            $db->createCommand()->truncateTable(MoneyCourse::tableName());
+
+            echo Console::startProgress(($row = 0), $total, ($startPrefix = 'Update MoneyCourse'));
+
+            foreach ($request->data['Valute'] as $item) {
+
+                $nominal = 1;
+                $moneyItem = new self([
+                    'name' => $item['Name'],
+                    'char' => $item['CharCode'],
+                    'val' => str_replace(',', '.', $item['Value']) / $nominal,
+                    'date' => \Yii::$app->formatter->asDatetime(time(), "Y-MM-dd H:i:s")
+                ]);
+
+                $moneyItem->save();
+
+                $row++;
+                echo Console::updateProgress($row, $total, $startPrefix . " {$moneyItem->char}");
+
+            }
+
+            echo Console::endProgress();
+
+            $transaction->commit();
+
+            echo "Update MoneyCourse success".PHP_EOL;
+
+        }catch (\Exception $exception){
+            $transaction->rollBack();
+        }
     }
 }
