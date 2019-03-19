757ef4c50684aaa104ecd8a3c5939e2fb4058e2d
alexandr <beller.a@mail.ru>
2019-03-19 15:34:51 +0700

    Примеры настроек

---
+++ exmaples/commonParamsExample.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = [
+    /**
+     * Вынести в глобальные параметры все возможные адреса системы
+     */
+    'domains.frontend'=>'http://frontend.net',
+    'domains.backend'=>'http://backend.net',
+];
+
+return $config;
