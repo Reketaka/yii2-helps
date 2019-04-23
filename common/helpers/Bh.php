5fab6caddf450cb0ee8fb270fcab599aa4229f17
alexandr <beller.a@mail.ru>
2019-04-23 16:01:10 +0700

    Добавление функций

--- common/helpers/Bh.php
+++ common/helpers/Bh.php
@@ -213,4 +213,10 @@ class Bh{
         return true;
 
     }
+
+    public static function mb_ucfirst($string, $enc = 'UTF-8')
+    {
+        return mb_strtoupper(mb_substr($string, 0, 1, $enc), $enc) .
+            mb_substr($string, 1, mb_strlen($string, $enc), $enc);
+    }
 }
