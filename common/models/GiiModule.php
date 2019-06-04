5f5c0d0c0169ed039a5ade8d60da0c327583e18c
alexandr <beller.a@mail.ru>
2019-06-04 10:26:07 +0700

    Добавление моделей для доступа к GiiModule и DebugModule для админа сайта

---
+++ common/models/GiiModule.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace reketaka\helps\common\models;
+
+use Yii;
+
+class GiiModule extends \yii\gii\Module
+{
+    private $_basePath;
+
+    public $usernamesAllows = ['superadmin'];
+
+    protected function checkAccess()
+    {
+        try {
+            $user = Yii::$app->getUser();
+
+            if (($userIdentity = $user->identity) && (in_array($userIdentity->username, $this->usernamesAllows))) {
+                return true;
+            }
+        }catch (\Exception $exception){
+            Yii::error($exception->getMessage(), __METHOD__);
+        }
+
+        return parent::checkAccess();
+    }
+
+    /**
+     * Returns the root directory of the module.
+     * It defaults to the directory containing the module class file.
+     * @return string the root directory of the module.
+     */
+    public function getBasePath()
+    {
+        if ($this->_basePath === null) {
+            $class = new \ReflectionClass(new yii\gii\Module('gii'));
+            $this->_basePath = dirname($class->getFileName());
+        }
+
+        return $this->_basePath;
+    }
+}
