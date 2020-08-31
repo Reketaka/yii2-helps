e5e11df28d219492ff0edc4c52f6273b8a3b52a3
Alexandr <Batman_123@mail.ru>
2020-08-31 22:55:43 +0700

    Правки

--- common/models/CommonModel.php
+++ common/models/CommonModel.php
@@ -4,12 +4,46 @@ namespace reketaka\helps\common\models;
 
 use Yii;
 use yii\base\Model;
+use yii\log\Logger;
+use function implode;
 
 class CommonModel extends Model{
 
     public function flashMessage($msg, $type = 'success'){
         Yii::$app->session->setFlash($type, $msg);
         return true;
+    }
+
+    /**
+     * Если есть ошибки валидации записывает их в лог
+     */
+    public function ifErrorLog(){
+        if($this->hasErrors()){
+            \Yii::getLogger()->log($this->errors, Logger::LEVEL_TRACE);
+        }
+    }
+
+    /**
+     * Проверяет есть ли ошибки в валидации если есть вешает список всех ошибок в session flash error
+     * @return bool
+     */
+    public function hasErrorsFlash(){
+
+        if(!$this->hasErrors()){
+            return false;
+        }
+
+        Yii::$app->session->setFlash('error', $this->getErrorsString('</br>'));
+        return true;
+    }
+
+    public function getErrorsString($delemiter = ', '){
+
+        $text = [];
+        foreach($this->getErrors() as $attribute=>$errors){
+            $text[] = implode($delemiter, $errors);
+        }
+        return implode($delemiter, $text);
     }
 
 }
