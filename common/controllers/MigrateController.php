f04a91e8c7f27170f9c9a4260391ddd71bf6b12d
Alexandr <Batman_123@Mail.ru>
2020-09-11 15:29:43 +0700

    Переопределение контроллера миграций

---
+++ common/controllers/MigrateController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace reketaka\helps\common\controllers;
+
+use yii\console\controllers\MigrateController as YiiMigrateController;
+
+class MigrateController extends YiiMigrateController{
+
+    public $onlyByPath = false;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function options($actionID)
+    {
+        return array_merge(
+            parent::options($actionID),
+            ['onlyByPath']
+        );
+    }
+
+    /**
+     * Returns the migrations that are not applied.
+     * @return array list of new migrations
+     */
+    protected function getNewMigrations()
+    {
+        $applied = [];
+        foreach ($this->getMigrationHistory(null) as $class => $time) {
+            $applied[trim($class, '\\')] = true;
+        }
+
+        $migrationPaths = [];
+        if (is_array($this->migrationPath)) {
+            foreach ($this->migrationPath as $path) {
+                $migrationPaths[] = [$path, ''];
+            }
+        } elseif (!empty($this->migrationPath)) {
+            $migrationPaths[] = [$this->migrationPath, ''];
+        }
+
+        if(!$this->onlyByPath) {
+            foreach ($this->migrationNamespaces as $namespace) {
+                $migrationPaths[] = [$this->getNamespacePath($namespace), $namespace];
+            }
+        }
+
+        $migrations = [];
+        foreach ($migrationPaths as $item) {
+            list($migrationPath, $namespace) = $item;
+            if (!file_exists($migrationPath)) {
+                continue;
+            }
+            $handle = opendir($migrationPath);
+            while (($file = readdir($handle)) !== false) {
+                if ($file === '.' || $file === '..') {
+                    continue;
+                }
+                $path = $migrationPath . DIRECTORY_SEPARATOR . $file;
+                if (preg_match('/^(m(\d{6}_?\d{6})\D.*?)\.php$/is', $file, $matches) && is_file($path)) {
+                    $class = $matches[1];
+                    if (!empty($namespace)) {
+                        $class = $namespace . '\\' . $class;
+                    }
+                    $time = str_replace('_', '', $matches[2]);
+                    if (!isset($applied[$class])) {
+                        $migrations[$time . '\\' . $class] = $class;
+                    }
+                }
+            }
+            closedir($handle);
+        }
+        ksort($migrations);
+
+        return array_values($migrations);
+    }
+
+    /**
+     * Returns the file path matching the give namespace.
+     * @param string $namespace namespace.
+     * @return string file path.
+     * @since 2.0.10
+     */
+    private function getNamespacePath($namespace)
+    {
+        return str_replace('/', DIRECTORY_SEPARATOR, \Yii::getAlias('@' . str_replace('\\', '/', $namespace)));
+    }
+
+
+}
