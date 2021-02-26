89a4be8978db2cb9cb8dab4d91a2244ddcaef685
Alexandr <Batman_123@mail.ru>
2021-02-26 21:56:48 +0700

    Добавление SingletonTrait

---
+++ common/models/SingletonTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace reketaka\helps\common\models;
+
+
+trait SingletonTrait{
+
+    public static function getInstance(){
+
+        $container = Yii::$container;
+
+        if($container->hasSingleton(self::class)){
+            return $container->get(self::class);
+        }
+
+
+        $container->setSingleton(self::class);
+
+        return $container->get(self::class);
+    }
+
+}
